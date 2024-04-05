4c6295e29398ff79ffb6cd030724252052d4def8
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-04-05 13:04:20 +0200

    feat: seed user id into project table

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Project;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -37,14 +38,17 @@ class ProjectSeeder extends Seeder
         
 
         $categories=Category::all()->pluck('id');
+        $user=User::all()->pluck('id');
         // $categories[]=null;
         
         for($i=0;$i<50;$i++){
             $category_id=$faker->randomElement($categories);
+            $user_id=$faker->randomElement($user);
             
             $project=new Project();
             $project->title=$faker->catchPhrase();
             $project->category_id=$category_id;
+            $project->user_id=$user_id;
             $project->description=$faker->paragraph(2);
             $project->link=$faker->url();
             $project->imageUrl=$faker->imageUrl(640, 480, 'animals', true);
